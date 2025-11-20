880bc93ee264ee7d912e5a2d9b51217436a0007f
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2025-11-20 15:10:48 +0100

    add bash scripts to launch runs

---
+++ SimG4Core/Application/test/runHGCalTest_nosubregions.sh
@@ -0,0 +1,15 @@
+localdir=$PWD
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SITECONFIG_PATH=/cvmfs/cms.cern.ch/SITECONF/T0_CH_CERN
+cmsrel CMSSW_15_1_0
+cd CMSSW_15_1_0/src
+cmsenv
+
+git cms-addpkg SimG4Core/Application
+git cms-addpkg SimG4Core/PhysicsLists
+git fetch my-cmssw
+git checkout my-cmssw/eprofile-allevents
+scram b -j4
+cd $localdir
+cmsRun CMSSW_15_1_0/src/SimG4Core/Application/test/HGCalTest_nosubregions.py
+
