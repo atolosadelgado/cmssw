82613ce42b16bf2909a82beba14444ab14cd9079
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2025-11-20 15:35:27 +0100

    adapt HGCal subregion bash run script to condor

--- SimG4Core/Application/test/runHGCalTest_nosubregions.sh
+++ SimG4Core/Application/test/runHGCalTest_nosubregions.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+set -e
+
+#set condor ids
+CLUSTERID=$1
+PROCID=$2
+
+if [ -z "$CLUSTERID" ] || [ -z "$PROCID" ]; then
+    echo "ERROR: This script requires 2 arguments: <ClusterId> <ProcId>"
+    exit 1
+fi
+
+git config --global user.github atolosadelgado
 localdir=$PWD
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SITECONFIG_PATH=/cvmfs/cms.cern.ch/SITECONF/T0_CH_CERN
@@ -11,5 +24,10 @@ git fetch my-cmssw
 git checkout my-cmssw/eprofile-allevents
 scram b -j4
 cd $localdir
+
+
 cmsRun CMSSW_15_1_0/src/SimG4Core/Application/test/HGCalTest_nosubregions.py
 
+BASE_OUTPUT_DIR="/afs/cern.ch/user/a/atolosad/work/HGCal/condor/test_nosubregions"
+JOB_OUTPUT_DIR="${BASE_OUTPUT_DIR}/${CLUSTERID}.${PROCID}"
+mv *.root $JOB_OUTPUT_DIR
